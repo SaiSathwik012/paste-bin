63e7c818d1f7939c947ae55c2ceb8d2c3e316494
Samudram Sai Sathwik <saisathwik012@gmail.com>
2026-01-04 12:57:21 +0530

    Updated code

--- models/Paste.js
+++ models/Paste.js
@@ -1,10 +1,26 @@
 import mongoose from 'mongoose';
 
-const PasteSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  content: { type: String, required: true },
-  expiresAt: { type: Date, default: null },
-  remainingViews: { type: Number, default: null },
-});
+const PasteSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      default: null,
+    },
+    remainingViews: {
+      type: Number,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.models.Paste || mongoose.model('Paste', PasteSchema);
+export default mongoose.models.Paste ||
+  mongoose.model('Paste', PasteSchema);
