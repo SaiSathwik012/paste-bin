13d9d787e1478def8fcab055675c8e6f4dd32b05
Samudram Sai Sathwik <saisathwik012@gmail.com>
2026-01-04 13:03:29 +0530

    Updated code

--- pages/api/pastes/index.js
+++ pages/api/pastes/index.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     console.log('Method :', req.method);
     if (req.method !== 'POST') {
         res.setHeader('Allow', ['POST']);
-        return res.status(405).json({ error: 'Method Not Allowed' });
+        return res.status(405).json({ error: 'Method Not Allowed', method: req.method });
     }
 
     try {
